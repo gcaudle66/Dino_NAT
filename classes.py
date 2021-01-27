51ffdda14fec94b5543d56794e6b0755403ad44f
Garrett Caudle <gcaudle66@gmail.com>
2021-01-27 04:02:34 -0600

    created show_ap_tag_summary text fsm template and it is working-to be used for validations after. Modified classes.py to contain the ninventory for wlc and able to push it in. All new functions in flow_ap_rename for connecting and pushing

---
+++ classes.py
@@ -0,0 +1,177 @@
+class Flow:
+    def __init__(self, who, what, when, where, why):
+        self.who = who
+        self.what = what
+        self.when = when
+        self.where = where
+        self.why = why
+
+
+class WLCinfo:
+    def __init__(self, device_name, device_host_ip, device_user, device_pass):
+        self.device_name = device_name
+        self.device_host_ip = device_host_ip
+        self.device_user = device_user
+        self.device_pass = device_pass
+
+    def output_args(self):
+        self.wlc_args_output = {
+            'device_name': self.device_name,
+            'device_host_ip': self.device_host_ip,
+            'device_user': self.device_user,
+            'device_pass': self.device_pass
+        }
+        return self.wlc_args_output
+
+    def create_inventory(self, *args, **kwargs):
+        self.wlc_ap_inventory = []
+        self.entry = []
+        print('---args---')
+        for arg in args:
+            self.entry = arg
+            self.wlc_ap_inventory.append(self.entry)
+        print('---kwargs---')
+        for key, value in kwargs.items():
+            self.entry = [key, value]
+            self.wlc_ap_inventory.append(self.entry)
+
+
+testwlc = WLCinfo('WLC-9800', '198.18.134.100', 'admin', 'C1sco12345')
+
+
+class Ap:
+    def __init__(self, apname, ethmac):
+        self.apname = apname
+        self.ethmac = ethmac
+        # Managed info
+        self.pri_cntr = ""
+        self.sec_cntr = ""
+        self.ip4_add = ""
+        self.dnac_managed = False
+        self.dnac_ip4_add = "unset"
+        # Tags
+        self.tag_RF = ""
+        self.tag_Site = ""
+        self.tag_Policy
+
+    def status_flags(self, *args, **kwargs):
+        self.flag_isAlive = False
+        self.flag_isRegistered = False
+        self.flag_isModified = False
+        self.flag_setReboot = False
+        self.flag_setDefault = False
+        self.flag_ackModified = False
+
+
+class AcPo:
+    """
+    This class defines the access point and all of its glory...
+    or at least all we are concerned about in this version of Dino
+    """
+
+    def __init__(self, apname, ethmac):
+        self.apname = apname
+        self.ethmac = ethmac
+
+    def configure(self, *args, **kwargs):
+        """
+        Config in this Method defines the current state of AP config.
+        """
+        self.pri_cntr = ""
+        self.sec_cntr = ""
+        self.ip4_add = ""
+        self.dnac_managed = False
+        self.dnac_ip4_add = "unset"
+        self.location = "default"
+
+    def tag_mapping(self):
+        self.tag_RF = ""
+        self.tag_Site = ""
+        self.tag_Policy = ""
+
+    def status_flags(self, *args, **kwargs):
+        """
+        The below flags are various markers to set for
+        different conditions based on events.
+            ##flaf_isAlive(Bool) Indicates True if Dino is able to
+            validate the AP is reachable
+            ##flag_isRegistered(Bool) True Indicates if the AP is currently
+             registered to a WLC
+            ##flag_isModified(Bool) True indicates that the current "Pre" state/config
+            for the AP has changed. NOTE: This state/config is an offline copy stored
+            in memory in Dino, not an active realtime config. Any changes would have
+            to be pushed for them to take affect
+            ##flag_setReboot(Tuple "(Bool, delay_time_value)") True indicate that the AP has
+            a reboot scheduled to occur. *args passed to tuple indicate timer till reload.
+            ##flag_setDefault(Bool) True indicate this AP has been flagged to perform a factory
+             default operation on the config. If Scheduled this flag works with flag_reboot
+            ##flag_ackModified(UNDECIDED) Idea is to have the user of Dino confirm the changes to
+        be pushed to any and all APs first, in which it would trigger this flag and log the event.
+        Still undecided if it will be used
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self.flag_isAlive = False
+        self.flag_isRegistered = False
+        self.flag_isModified = False
+        self.flag_setReboot = False
+        self.flag_setDefault = False
+        self.flag_ackModified = False
+
+    def ap_state(self):
+        """
+        This func controls the current and recent
+        state/config of the AP to be used for
+        sanity and completion checks. State is
+        copied to the class if modifications are
+        slated for this AP . Should a pre-post check
+        fail either by seeing unintented state values
+        or another validation check...this app will
+        error out and dump to a log "why".
+        """
+        # state_data is a dict containing all data stored for
+        # object as gathered since last poll. This is "PRE" config
+        # data for check
+        self.pre_state_data = {}
+
+        def __check__(self):
+            """
+            AP's !! PRE modifaction !! data current state/config data stored
+            To be used for sanity comparisons and atomic checks and compare
+            to the POST modification for "desired state" checks
+            """
+            self.pre_state_data
+
+        def __update__(self):
+            """
+            Force polling of APs controller device for new state info
+            related to AP
+            """
+
+
+class ConnexList(object):
+    def __init__(self, match_list):
+        self.connexList = match_list
+        self.connexArgs = {}
+        self.sendCmds = []
+
+    def __enter__(self):
+        self.connexList = match_list.copy()
+        self.ConnexArgs = self.setConnArgs(self)
+        return self.connexList, ConnexArgs
+
+    def setConnArgs(self):
+        import getpass
+        self.connexArgs = {"ip": self.connexList[0]}
+        return self.connexArgs
+
+    def getConnArgs(self):
+        self.getConnArgs = print(self.connexArgs)
+
+    def getCmds(self):
+        self.sendCmds = ap_rename20.api_create_commands(connexList)
+        return self.sendCmds
+
+    def __exit__(self):
+        self.connexList.clear()
--- flows/flow_ap_rename.py
+++ flows/flow_ap_rename.py
@@ -1,36 +1,33 @@
 import netmiko as nm
 import time
 
-net_connect = nm.BaseConnection
+connIsAlive = False
 
 
 def connex(*args, **kwargs):
     try:
-        net_connect = nm.BaseConnection(ip=kwargs.get(device_host_ip), username=kwargs.get(device_user)
-, password=kwargs.get(device_pass), session_log=f"ssh_session_logfile.txt", session_log_file_mode="write", session_log_record_writes="True")
+        conn = nm.BaseConnection(ip=kwargs.get(device_host_ip), username=kwargs.get(device_user), password=kwargs.get(
+            device_pass), session_log=f"ssh_session_logfile.txt", session_log_file_mode="write", session_log_record_writes="True")
     except nm.NetMikoTimeoutException:
         print("*********************************************************\n")
         print("* ERROR: Connection to {} timed-out.     \n".format('wlc'))
         print("* Skipping this entry for now...                              ")
         print("*********************************************************\n")
     else:
-        connIsAlive = net_connect.is_alive()
-        return net_connect
+        connIsAlive = conn.is_alive()
+        return conn
 
 
 def getApInventory():
     """ Here we will run commands against the WLC to gather \n""" \
-        """ necessary data for comparison against CSV           \n"""
-    while connIsAlive is False:
-        print("SSH Connection to WLC has closed. Reconnecting....")
-        conn = connect()
-    try:
-        cli_output = conn.send_command_timing("show ap config general | include ^Cisco AP Name|^MAC Address", use_textfsm=True, textfsm_template="./templates/cisco_ios_show_ap_template-v2_5.textfsm")
-    except nm.NetMikoTimeoutException as err:
-        print("Timeout Error occured. Timeout waiting for device for an operation to continue.")
-        return err.with_traceback
-# except:
-# print("Unexpected error:", sys.exc_info()[0])
-# raise
-    else:
-        return cli_output
+    """ necessary data for comparison against CSV           \n"""
+    cli_output = conn.send_command_timing("show ap config general | include ^Cisco AP Name|^MAC Address", use_textfsm=True, textfsm_template="./templates/cisco_ios_show_ap_template-v3.textfsm")
+
+
+def get_ap_tags():
+    cli_output = conn.send_command_timing("show ap tag summary",
+                                    use_textfsm=True, textfsm_template="./templates/cisco_ios_show_ap_tag_summary_template-v1.textfsm")
+
+
+def set_ap_tags():
+    print('**Warning: "Associating/Disassociating tags will cause associated AP to reconnect; Roughly 30 seconds AP will be offline**')
